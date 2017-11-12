b67e0d05b1abd52cdc1b1e0cb97ead7502795a61
kilogate <kilogate@163.com>
2017-11-12 19:16:40 +0800

    进入客户列表页面

--- chapter2/src/main/java/com/kilogate/chapter2/controller/CustomerServlet.java
+++ chapter2/src/main/java/com/kilogate/chapter2/controller/CustomerServlet.java
@@ -1,25 +1,38 @@
 package com.kilogate.chapter2.controller;
 
+import com.kilogate.chapter2.model.Customer;
+import com.kilogate.chapter2.service.CustomerService;
+
 import javax.servlet.ServletException;
 import javax.servlet.annotation.WebServlet;
 import javax.servlet.http.HttpServlet;
 import javax.servlet.http.HttpServletRequest;
 import javax.servlet.http.HttpServletResponse;
 import java.io.IOException;
+import java.util.List;
 
 /**
- * 客户列表
+ * 客户管理
  *
  * @author fengquanwei
  * @create 2017/11/11 15:56
  **/
 @WebServlet("/customer")
 public class CustomerServlet extends HttpServlet {
+    private CustomerService customerService;
+
+    @Override
+    public void init() throws ServletException {
+        customerService = new CustomerService();
+    }
+
     /**
      * 进入客户列表页面
      */
     @Override
-    protected void doGet(HttpServletRequest req, HttpServletResponse resp) throws ServletException, IOException {
-        // TODO
+    protected void doGet(HttpServletRequest request, HttpServletResponse response) throws ServletException, IOException {
+        List<Customer> customerList = customerService.getCustomerList();
+        request.setAttribute("customerList", customerList);
+        request.getRequestDispatcher("/WEB-INF/view/customer.jsp").forward(request, response);
     }
 }
